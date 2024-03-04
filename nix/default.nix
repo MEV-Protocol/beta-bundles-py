e71483706127416ecdbcea0fda039ed5f97d1655
Samuel Rounce <srounce@users.noreply.github.com>
2024-03-04 13:06:00 +0000

    WIP: package with Nix (#2)
    * WIP: package with Nix
    
    * WIP: nix build working
    
    * chore: make flake-parts use flake's nixpkgs version
    
    * chore: add .envrc
    
    * feat: add declare docker image in Nix flake

---
+++ nix/default.nix
@@ -0,0 +1,24 @@
+{ ... }: {
+  perSystem = { pkgs, ... }:
+  let
+    app = pkgs.callPackage ./pkgs/beta-bundles.nix { };
+  in
+  {
+    packages = {
+      default = app;
+
+      beta-bundles-py = app;
+      beta-bundles-py-docker = app.docker;
+    };
+
+    devShells.default = app.devShell;
+
+    treefmt = {
+      projectRootFile = "../flake.nix";
+
+      programs.black.enable = true;
+      programs.nixfmt.enable = true;
+    };
+  };
+}
+
