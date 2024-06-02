4f11888764533e2bf2fd7e09af44839346cf2eda
sandybradley <sandy.bradley@gmail.com>
2024-06-02 22:04:58 +0200

    chore: updates

--- beta_bundles_py/main.py
+++ beta_bundles_py/main.py
@@ -44,6 +44,9 @@ bidder_abi = json.load(f3)["abi"]
 bidder = w3.eth.contract(address=os.environ.get("BIDDER"), abi=bidder_abi)
 f3.close()
 
+sig_auction_closed = w3.keccak(text='AuctionSettled(uint256)').hex()
+sig_auction_opened = w3.keccak(text='AuctionOpened(uint256,uint120)').hex()
+
 def submitBundle(slot: int, txs: list):
     headers = {'Content-Type': 'application/json'}
     req = {
@@ -65,14 +68,11 @@ def submitBundle(slot: int, txs: list):
 def handle_event(event):
     logger.info(event)
     if event["address"] == os.environ.get("AUCTIONEER"):
-        signature = w3.keccak(text='AuctionSettled(uint256)').hex()
-        signature0 = w3.keccak(text='AuctionOpened(uint256,uint120)').hex()
-        logger.debug(signature)
-        logger.debug(signature0)
-        if signature0 == event["topics"][0].hex():
+        if sig_auction_opened == event["topics"][0].hex():
+            # auction opened
             slot = int(event["topics"][1].hex(), 16)
             logger.debug(slot)
-            # submit bundle
+            # build private tx from env vars
             args = json.loads(os.environ.get("TX_ARGS"))
             logger.debug(args)
             sig = os.environ.get("TX_SIG")
@@ -85,7 +85,6 @@ def handle_event(event):
             # construct tx
             block = w3_l1.eth.get_block('latest')
             base_fee = block.baseFeePerGas * 2
-            
             transaction = {
                 'chainId': int(os.environ.get("CHAIN_ID")),
                 'from': os.environ.get("CALLER"),
@@ -100,19 +99,22 @@ def handle_event(event):
             logger.debug(transaction)
             # cannot estimate gas as 
             amount_of_gas = w3_l1.eth.estimate_gas(transaction)
-            
             transaction["gas"] = amount_of_gas
 
-            # 2. Sign tx with a private key
+            # Sign tx with a private key
             signed = w3_l1.eth.account.sign_transaction(transaction, os.environ.get("PRIVATE_KEY"))
             logger.debug(signed.rawTransaction.hex())
             
             # submit bundle to rpc
             hash = submitBundle(slot, [signed.rawTransaction.hex()])
             logger.debug(hash)
-            
-            bidder.functions.openBid(os.environ.get("WEI_PER_GAS"), amount_of_gas, [hash]).transact()
-        elif signature == event["topics"][0].hex():
+            # open bid on OpenBidder contract
+            wei_per_gas = int(os.environ.get("WEI_PER_GAS"))
+            tx = bidder.functions.openBid(wei_per_gas, amount_of_gas, hash).build_transaction({"from": os.environ.get("CALLER"), "value": wei_per_gas * amount_of_gas, "nonce": w3.eth.get_transaction_count(os.environ.get("CALLER"))})
+            signed_tx = w3.eth.account.sign_transaction(tx, private_key=os.environ.get("PRIVATE_KEY"))
+            tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+            w3.eth.wait_for_transaction_receipt(tx_hash)
+        elif sig_auction_closed == event["topics"][0].hex():
             # Auction settled, decode slot number
             slot = int(event["topics"][1].hex(), 16)
             # check futures balance
@@ -120,7 +122,10 @@ def handle_event(event):
             logger.debug(bal)
             if bal > 0:
                 # finalise bundle submission on L2
-                bidder.functions.submitBundle(slot).transact()
+                tx = bidder.functions.submitBundles(slot).build_transaction({"from": os.environ.get("CALLER"), "nonce": w3.eth.get_transaction_count(os.environ.get("CALLER"))})
+                signed_tx = w3.eth.account.sign_transaction(tx, private_key=os.environ.get("PRIVATE_KEY"))
+                tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+                w3.eth.wait_for_transaction_receipt(tx_hash)
 
 def log_loop(poll_interval):
     while True:
@@ -129,9 +134,7 @@ def log_loop(poll_interval):
         time.sleep(poll_interval)
 
 def main():
-    setup_logging()
     log_loop(2)
 
 if __name__ == '__main__':
-    # setup_logging()
     main()
