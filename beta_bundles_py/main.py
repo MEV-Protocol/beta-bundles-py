a06cd70999f9e3b490852964fbbde7ffd9846f55
sandybradley <sandy.bradley@gmail.com>
2024-06-02 11:47:19 +0200

    chore: update

--- beta_bundles_py/main.py
+++ beta_bundles_py/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from web3 import Web3, HTTPProvider
+from eth_abi import encode
 import time
 import os
 import json
@@ -8,31 +9,29 @@ import requests
 import logging
 import coloredlogs
 
-def setup_logging(log_level=logging.DEBUG):
-    """Setup root logger and quiet some levels."""
-    logger = logging.getLogger()
+log_level=logging.DEBUG
+# """Setup root logger and quiet some levels."""
+logger = logging.getLogger()
 
-    # Set log format to display the logger name to hunt down verbose logging modules
-    fmt = "%(name)-25s %(levelname)-8s %(message)s"
+# Set log format to display the logger name to hunt down verbose logging modules
+fmt = "%(name)-25s %(levelname)-8s %(message)s"
 
-    # Use colored logging output for console with the coloredlogs package
-    # https://pypi.org/project/coloredlogs/
-    coloredlogs.install(level=log_level, fmt=fmt, logger=logger)
+# Use colored logging output for console with the coloredlogs package
+# https://pypi.org/project/coloredlogs/
+coloredlogs.install(level=log_level, fmt=fmt, logger=logger)
 
-    # Disable logging of JSON-RPC requests and replies
-    logging.getLogger("web3.RequestManager").setLevel(logging.WARNING)
-    logging.getLogger("web3.providers.HTTPProvider").setLevel(logging.DEBUG)
-    # logging.getLogger("web3.RequestManager").propagate = False
+# Disable logging of JSON-RPC requests and replies
+logging.getLogger("web3.RequestManager").setLevel(logging.WARNING)
+logging.getLogger("web3.providers.HTTPProvider").setLevel(logging.DEBUG)
+# logging.getLogger("web3.RequestManager").propagate = False
 
-    # Disable all internal debug logging of requests and urllib3
-    # E.g. HTTP traffic
-    logging.getLogger("requests").setLevel(logging.DEBUG)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-
-    return logger
+# Disable all internal debug logging of requests and urllib3
+# E.g. HTTP traffic
+logging.getLogger("requests").setLevel(logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 # instantiate Web3 instance, connecting to L2
-w3 = Web3(HTTPProvider(os.environ.get("L2_RPC", "https://holesky-api.securerpc.com/l2/")))
+w3 = Web3(HTTPProvider(os.environ.get("RPC_L2", "https://holesky-api.securerpc.com/l2/")))
 w3_l1 = Web3(HTTPProvider(os.environ.get("BETA_BUNDLE_RPC")))
 
 f = open('abis/Auctioneer.json')
@@ -40,20 +39,13 @@ auctioneer_abi = json.load(f)["abi"]
 auctioneer = w3.eth.contract(address=os.environ.get("AUCTIONEER"), abi=auctioneer_abi)
 f.close()
 
-f2 = open('abis/SettlementHouse.json')
-settlement_abi = json.load(f2)["abi"]
-settlement = w3.eth.contract(address=os.environ.get("SETTLEMENT"), abi=settlement_abi)
-f2.close()
-
-f3 = open('abis/MockBidder.json')
+f3 = open('abis/OpenBidder.json')
 bidder_abi = json.load(f3)["abi"]
 bidder = w3.eth.contract(address=os.environ.get("BIDDER"), abi=bidder_abi)
 f3.close()
 
 def submitBundle(slot: int, txs: list):
-    # payload = "{\n  \"method\": \"manifold_sendBundle\",\n  \"id\": 1,\n  \"jsonrpc\": \"2.0\",\n  \"params\": [\n  {  \"txs\":  [\""+signedTxRaw+"\"] , \"blockNumber\":  \""+hex(chain.height + 10)+"\" }  ]\n}"
     headers = {'Content-Type': 'application/json'}
-
     req = {
         "jsonrpc": "2.0",
         "method": "mev_sendBetaBundle",
@@ -66,66 +58,69 @@ def submitBundle(slot: int, txs: list):
         "id": 1
     }
     response = requests.post(os.environ.get("BETA_BUNDLE_RPC"), headers=headers, data = json.dumps(req))
-    # print(response)
     res = response.json()
-    # print(res)
     return res["result"]
     
     
 def handle_event(event):
-    # print(event)
-    # print(event["address"])
-    # print(os.environ.get("AUCTIONEER"))
+    logger.info(event)
     if event["address"] == os.environ.get("AUCTIONEER"):
         signature = w3.keccak(text='AuctionSettled(uint256)').hex()
-        # print(signature)
-        if signature == event["topics"][0].hex():
+        signature0 = w3.keccak(text='AuctionOpened(uint256,uint120)').hex()
+        logger.debug(signature)
+        logger.debug(signature0)
+        if signature0 == event["topics"][0].hex():
+            slot = int(event["topics"][1].hex(), 16)
+            logger.debug(slot)
+            # submit bundle
+            args = json.loads(os.environ.get("TX_ARGS"))
+            logger.debug(args)
+            sig = os.environ.get("TX_SIG")
+            x = sig.find("(")
+            y = sig.find(")")
+            arg_types = sig[x+1:y].split(",")
+            logger.debug(arg_types)
+            data = w3.keccak(text=sig)[0:4] + encode(arg_types, args) 
+            logger.debug(data)
+            # construct tx
+            block = w3_l1.eth.get_block('latest')
+            base_fee = block.baseFeePerGas * 2
+            
+            transaction = {
+                'chainId': int(os.environ.get("CHAIN_ID")),
+                'from': os.environ.get("CALLER"),
+                'to': os.environ.get("TX_TO"),
+                'value': int(os.environ.get("TX_VALUE")),
+                'nonce': w3_l1.eth.get_transaction_count(os.environ.get("CALLER")),
+                'maxPriorityFeePerGas': 0,
+                'data': data,
+                'gas': 1000000,
+                'maxFeePerGas': base_fee
+            }
+            logger.debug(transaction)
+            # cannot estimate gas as 
+            amount_of_gas = w3_l1.eth.estimate_gas(transaction)
+            
+            transaction["gas"] = amount_of_gas
+
+            # 2. Sign tx with a private key
+            signed = w3_l1.eth.account.sign_transaction(transaction, os.environ.get("PRIVATE_KEY"))
+            logger.debug(signed.rawTransaction.hex())
+            
+            # submit bundle to rpc
+            hash = submitBundle(slot, [signed.rawTransaction.hex()])
+            logger.debug(hash)
+            
+            bidder.functions.openBid(os.environ.get("WEI_PER_GAS"), amount_of_gas, [hash]).transact()
+        elif signature == event["topics"][0].hex():
             # Auction settled, decode slot number
             slot = int(event["topics"][1].hex(), 16)
-            # print(slot)
             # check futures balance
             bal = auctioneer.functions.balanceOf(os.environ.get("BIDDER"), slot).call()
-            # print(bal)
+            logger.debug(bal)
             if bal > 0:
-                # submit bundle
-                # construct tx
-                transaction = {
-                    'chainId': 17000,
-                    'from': os.environ.get("CALLER"),
-                    'to': os.environ.get("CALLER"),
-                    'value': 1000000000,
-                    'nonce': w3_l1.eth.get_transaction_count(os.environ.get("CALLER")),
-                    'gas': 50000,
-                    'maxFeePerGas': 2000000000,
-                    'maxPriorityFeePerGas': 1000000000,
-                }
-                # print(transaction)
-
-                # 2. Sign tx with a private key
-                signed = w3_l1.eth.account.sign_transaction(transaction, os.environ.get("PRIVATE_KEY"))
-                # print(signed.rawTransaction.hex())
-                
-                # submit bundle to rpc
-                hash = submitBundle(slot, [signed.rawTransaction.hex()])
-                # print(hash)
-                
                 # finalise bundle submission on L2
-                # settlement.functions.submitBundle(slot, bal, [hash]).transact()
-                tx = bidder.functions.submit(slot, bal, [bytes.fromhex(hash[2:])]).build_transaction(
-                    {
-                        'chainId': 42169,
-                        'gas': 300000,
-                        'maxFeePerGas': w3.to_wei('5', 'gwei'),
-                        'maxPriorityFeePerGas': w3.to_wei('1', 'gwei'),
-                        'nonce': w3.eth.get_transaction_count(os.environ.get("CALLER")),
-                    })
-                # print(tx)
-                signed_tx = w3.eth.account.sign_transaction(tx, os.environ.get("PRIVATE_KEY"))
-                # print(signed_tx)
-                tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-                # print(tx_hash)
-                tx = w3.eth.get_transaction(tx_hash)
-                # print(tx)
+                bidder.functions.submitBundle(slot).transact()
 
 def log_loop(poll_interval):
     while True:
@@ -138,4 +133,5 @@ def main():
     log_loop(2)
 
 if __name__ == '__main__':
+    # setup_logging()
     main()
